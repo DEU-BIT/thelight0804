e50fb5e021c2e610ad0c97539013634daedf5c69
thelight0804 <thelight0804@gmail.com>
2021-05-18 18:47:52 +0900

    제곱과 몫 연산자 추가

--- 210518_input, if.py
+++ 210518_input, if.py
@@ -1,4 +1,9 @@
 i=int(input("숫자를 입력해주세요"))
 print ("i : ", i)
+
 if 10 < i < 100:
     print("i는 10과 100사이의 숫자입니다")
+elif i>=100:
+    print("i는 100을 초과합니다")
+else:
+    print("i는 10 이하입니다")
---
+++ 210518_제곱, 몫연산자.py
@@ -0,0 +1,6 @@
+a = int(input("첫번째 숫자 하나를 입력해 주세요 : "))
+b = int(input("두번째 숫자 하나를 입력해 주세요 : "))
+
+print(a ** b) # ** : 제곱연산자
+print(a // b) # // : 몫
+print(a / b)
